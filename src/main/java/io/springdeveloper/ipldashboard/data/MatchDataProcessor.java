b04f19fccbf63722dc24f9b4f571c12ae731f689
Het shah <hetshah3008@gmail.com>
2021-07-20 17:16:06 -0400

    Used Spring batch to import the data

---
+++ src/main/java/io/springdeveloper/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,55 @@
+ package io.springdeveloper.ipldashboard.data;
+
+import java.time.LocalDate;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import io.springdeveloper.ipldashboard.model.Match;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+  private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+  @Override
+  public Match process(final MatchInput matchInput) throws Exception {
+
+    Match match=new Match();
+    match.setId(Long.parseLong(matchInput.getId()));
+    match.setCity(matchInput.getCity());
+    match.setDate(LocalDate.parse(matchInput.getDate()));
+    match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+
+
+   // set team1 and team2 depend on team orders 
+   String firstInningsTeam, secondInningsTeam;
+  if("bat".equals(matchInput.getToss_decision()))
+  {
+      firstInningsTeam=matchInput.getToss_winner();
+      secondInningsTeam=matchInput.getToss_winner().equals(match.getTeam1())
+       ?matchInput.getTeam2():matchInput.getTeam1();
+  }
+  else{
+      secondInningsTeam=matchInput.getToss_winner();
+      firstInningsTeam=matchInput.getToss_winner().equals(match.getTeam1())
+       ?matchInput.getTeam2():matchInput.getTeam1();      
+  }
+  match.setTeam1(firstInningsTeam);
+  match.setTeam2(secondInningsTeam);
+ match.setTossWinner(matchInput.getToss_winner());
+ match.setTossDecision(matchInput.getToss_decision());
+ match.setResult(matchInput.getResult());
+ match.setResultMargin(matchInput.getResult_margin());
+ match.setUmpire1(match.getUmpire1());
+ match.setUmpire2(match.getUmpire2());
+
+
+
+    return match;  
+  }
+
+}
+
