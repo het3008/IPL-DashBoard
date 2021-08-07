82629e4103d3ea7a7bd78d734e4dd7dd74297aef
Het shah <hetshah3008@gmail.com>
2021-08-07 13:42:49 -0400

    Build the API for Team Information

---
+++ src/main/java/io/springdeveloper/ipldashboard/controller/TeamController.java
@@ -0,0 +1,39 @@
+package io.springdeveloper.ipldashboard.controller;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import io.springdeveloper.ipldashboard.model.Team;
+import io.springdeveloper.ipldashboard.repository.MatchRepository;
+import io.springdeveloper.ipldashboard.repository.TeamRepository;
+
+@RestController
+
+public class TeamController {
+
+private TeamRepository teamRepository;
+private MatchRepository matchRepository;
+
+
+public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+    this.teamRepository = teamRepository;
+    this.matchRepository=matchRepository;
+
+}
+   
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName)
+     {
+  Team team=this.teamRepository.findByTeamName(teamName); 
+  team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4)); 
+  return team; 
+    }
+
+
+    
+}
